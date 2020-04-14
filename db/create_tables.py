662a1f18cb729b165b527ba1664b0d33d3d93e43
pawel-zajac90 <zajac.pawel@protonmail.com>
2020-04-14 02:52:58 +0200

    Tables and example values changed

--- db/create_tables.py
+++ db/create_tables.py
@@ -1,29 +1,52 @@
 import sqlite3
-from config import db_path
+db_path = 'cocktails.db'
 
 
 # Create tables
 def create(cur):
+    # Pubs table
     cur.execute("""
         CREATE TABLE IF NOT EXISTS Pubs (
         pub_id INTEGER PRIMARY KEY ASC,
-        pub_name varchar(25) NOT NULL);
+        pub_name VARCHAR NOT NULL);
         """)
 
+    # Cocktails table
     cur.execute('''
         CREATE TABLE IF NOT EXISTS Cocktails (
-        drink_id INTEGER PRIMARY KEY ASC,
-        drink_name varchar(25),
-        pub_id int,
-        rate int(5));
+        cocktail_id INTEGER PRIMARY KEY ASC,
+        cocktail_name VARCHAR NOT NULL,
+        pub_id INTEGER NOT NULL,
+        FOREIGN KEY (pub_id) REFERENCES Pubs(pub_id));
         ''')
 
+    # Ratings table
     cur.execute('''
-        CREATE TABLE IF NOT EXISTS Rating (
+        CREATE TABLE IF NOT EXISTS Rates (
         rate_id INTEGER PRIMARY KEY ASC,
-        drink_id int,
-        pub_id int NOT NULL,
-        rate int(5));
+        cocktail_id INTEGER NOT NULL,
+        pub_id INTEGER NOT NULL,
+        rate INTEGER,
+        FOREIGN KEY (cocktail_id) REFERENCES Cocktails(cocktail_id),
+        FOREIGN KEY (pub_id) REFERENCES Pubs(pub_id));
+        ''')
+
+    # Passwords table
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Passwords (
+        pass_id INTEGER PRIMARY KEY ASC,
+        user_id INTEGER NOT NULL,
+        hash VARCHAR NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES Users(user_id));
+        ''')
+
+    # Users Table
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+        user_id INTEGER PRIMARY KEY ASC,
+        login VARCHAR NOT NULL,
+        email VARCHAR NOT NULL,
+        modified_on VARCHAR);
         ''')
     return True
 
