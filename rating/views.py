ef9f0eba6af6652069d01b2fb809b46d8c26314d
pawel-zajac90 <zajac.pawel@protonmail.com>
2020-07-06 00:05:28 +0200

    comments addded

--- rating/views.py
+++ rating/views.py
@@ -3,8 +3,7 @@ from .models import Pub, Cocktail, Rating
 from .forms import NewPubForm, NewCocktailForm
 from cocktails_rating import db
 from flask import redirect, request
-from flask_sqlalchemy import SQLAlchemy
-from flask import make_response, redirect, url_for, session, jsonify
+from flask import make_response, redirect, url_for, jsonify
 from helpers.does_record_exist import does_not_exist, does_exist
 
 
@@ -55,24 +54,30 @@ def cocktail():
     return response
 
 
+# GET list of all cocktails with their avarege rating
+# or
+# POST new rating.
 @rating.route('/rating', methods=['GET', 'POST'])
 def cocktails_rating():
-    if request.method =='GET':
+    if request.method == 'GET':
         if Cocktail.query.first() is None:
-            return does_not_exist
+            return does_not_exist()
 
         cocktails_list = []
-        for c in Cocktail.query.all():
-            ratings = 0
-            for rating in c.ratings:
-                ratings += rating.rating
-            avg = ratings / len(c.ratings)
+        for cocktail in Cocktail.query.all():
+            sum_of_ratings = 0
+            for rating in cocktail.ratings:
+                sum_of_ratings += rating.rating
+            try:
+                avg = sum_of_ratings / len(cocktail.ratings)
+            except ZeroDivisionError:
+                avg = 0
             cocktails_list.append(
-                {'cocktail_id': c.id, 'cocktail_name': c.name, 'description': c.description,
+                {'cocktail_id': cocktail.id, 'cocktail_name': cocktail.name, 'description': cocktail.description,
                  'average_rating': avg})
         response = make_response(jsonify(cocktails_list), 200)
         return response
-
+# To add new rating 'rating' and 'cocktail_id' in request body required.
     elif request.method == 'POST':
         if request.json is None:
             return redirect(url_for('rating.rating'), 400)
@@ -89,6 +94,8 @@ def cocktails_rating():
         return redirect(url_for('rating.rating'), 200)
 
 
+# Add new pub to database - moderate or higher permission required.
+# Request need to contain data in forms.
 @rating.route('/add/pub', methods=['POST'])
 def add_pub():
     form = NewPubForm()
@@ -101,6 +108,8 @@ def add_pub():
     return redirect(url_for('rating.index'), 300)
 
 
+# Add new cocktail to database - moderate or higher permission required
+# Request need to contain data in forms.
 @rating.route('/add/cocktail', methods=['POST'])
 def add_cocktail():
     form = NewCocktailForm()
@@ -109,8 +118,10 @@ def add_cocktail():
     description = form.description.data
     if Cocktail.query.filter_by(name=name).first() is not None:
         return does_exist()
+
     elif Pub.query.filter_by(id=id).first() is None:
         return does_not_exist()
+
     cocktail = Cocktail(name=name, pub_id=id, description=description)
     db.session.add(cocktail)
     db.session.commit()
